52863b31f5708bca61739198d4ed2e15bf1a7d3e
deepuk <deepukdharan@yahoo.in>
2019-06-02 13:57:22 +0530

    Signed-off-by: deepuk <deepukdharan@yahoo.in>

--- src/app/viewprojects/viewprojects.component.spec.ts
+++ src/app/viewprojects/viewprojects.component.spec.ts
@@ -1,10 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ViewprojectsComponent } from './viewprojects.component';
+import { projects } from 'src/app/Models/projects';
+
+
+import {projectDetails} from '../Models/projects';
+
+import {TaskmanagerserviceService} from '../shared/taskmanagerservice.service';
+
+import { Observable , from} from 'rxjs';
 
 describe('ViewprojectsComponent', () => {
   let component: ViewprojectsComponent;
   let fixture: ComponentFixture<ViewprojectsComponent>;
+  let service : TaskmanagerserviceService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -19,7 +28,32 @@ describe('ViewprojectsComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should return project details', () => {
+
+    const projectDetails : project[] = [
+      {
+        project_id = 1,
+    project:'Universal 1',
+    startdate:'2019-03-02',
+    enddate:'2019-10-10',
+    priority:1,
+    numberoftasks:1,
+    completed:'Yes'
+      }
+    ];
+
+    spyOn(service,'GetAllProjects').and.callFake(() =>
+      {
+        return from([projectDetails]);
+      }
+    );
+
+    component.ngOnInit();
+
+    expect(component.projectDetails).toEqual(projectDetails);
+
+     
+
     expect(component).toBeTruthy();
   });
 });
